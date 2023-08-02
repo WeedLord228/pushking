92e3373e16ab760d1d65881276597393bf293af9
WeedLord228 <zajtzevar@gmail.com>
2023-08-02 20:55:33 +0500

    Data cleaning and more replacements added in data preparation

--- more_data_preparation_nlfi.py
+++ more_data_preparation_nlfi.py
@@ -4,15 +4,33 @@ with open("materials/more_data.txt") as file:
     lines = file.readlines()
 
 raw_lines = [x for x in lines if x.startswith("\t\t")]
-no_dia_lines = replace_in_all_lines(r"&#....", "", raw_lines)
-data = replace_in_all_lines(r"\xa0", " ", no_dia_lines)
+replacements = [
+    (r"&#....", ""),
+    (r"\xa0", " "),
+    ("…", "..."),
+    ("–", "-"),
+    ("—", "-"),
+    ("-", "-"),
+    ("«", '"'),
+    ("»", '"'),
+    ("„", '"')
+]
+
+data = raw_lines
+for replacement in replacements:
+    data = replace_in_all_lines(replacement[0], replacement[1], data)
+
 data = [x[2:] for x in data]
+
+with open('materials/sentences_nlfi_raw.txt', "w", encoding="UTF-8") as file:
+    for line in data:
+        file.write(line)
 
 train_threshold = round((len(data) / 100) * 80)
 
 # nlfi - number of lines, french included
-TRAIN_FILE_NAME = "materials/train_nlfi.txt"
-EVAL_FILE_NAME = "materials/eval_nlfi.txt"
+TRAIN_FILE_NAME = "materials/train_nlfi_raw.txt"
+EVAL_FILE_NAME = "materials/eval_nlfi_raw.txt"
 
 with open(TRAIN_FILE_NAME, "w", encoding="UTF-8") as file:
     for line in data[:train_threshold]:
